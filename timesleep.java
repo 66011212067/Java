ecea41f6c859e3035190ff1d5f476b66eee67deb
IT405 <>
2023-12-26 16:23:55 +0700

    whk06

--- timesleep.java
+++ timesleep.java
@@ -1,15 +1,34 @@
+import java.util.Scanner;
 public class timesleep {
     public static void main(String[] args) {
-        for(int i=24;;i--){
+        int y=0,m=0,d=0;
+        y = input("input year: ");
+        m = input("input month: ");
+        d = input("input day: ");
+        System.out.println("YEAR:\tMONTH:\tDAY:");
+        for(;d>=0;d--){
             try{
-                Thread.sleep(100);
-                System.out.print("\r"+i+" ");
-                if(i==0){
-                    i = 24;
-                }
-            }catch(InterruptedException err){
-                System.out.println(err);
+                Thread.sleep(50);
+                System.out.printf("\r%d \t%d \t%d ",y,m,d);
+            }catch(InterruptedException err){System.out.println(err);}
+            if(d==0&&m!=0){
+                d=30;
+                m -= 1;
             }
+            if(m==0&&y!=0){
+                m=12;
+                y -= 1;
+            }
+            if(y<0){break;}
         }
+        System.out.println("\n.....BOOOOMMMMM.....");
+        System.out.println("...HAPPY NEW YEAR...");
+    }
+
+    static int input(String title){
+        Scanner sc = new Scanner(System.in);
+        System.out.print(title);
+        int number = (int) sc.nextInt();
+        return number;
     }
 }
