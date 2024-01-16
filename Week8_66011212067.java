e32a3acb889d6c41eb25e11bc1e91d5802620a24
Passapol <phasphlsuthahthrrm976@gmail.com>
2024-01-16 18:03:52 +0700

    Update Week8_66011212067.java

--- Week8_66011212067.java
+++ Week8_66011212067.java
@@ -6,15 +6,15 @@ public class Week8_66011212067 {
         int n = sc.nextInt();
 
         int metic[][] = new int[n][n];
-        float avg[] = new float[n];
+        float average[][] = new float[n][n];
         for(int i1=0;i1<metic.length;i1++){
-            int avgs=0;
+            int avg=0;
             for(int i2=0;i2<metic.length;i2++){
                 int random = (int) (Math.random()*10+1);
                 metic[i1][i2] = random;
-                avgs = avgs+random ;
+                avg = avg+random ;
             }
-            avg[i1] = avgs/n;
+            average[i1][i1] = avg/n;
         }
         System.out.print("DATA: \n");
         for(int i1=0;i1<metic.length;i1++){
@@ -24,12 +24,12 @@ public class Week8_66011212067 {
             System.out.print("\n");
         }
         System.out.print("DATAAVERAGE: \n");
-        for(int i1=0;i1<metic.length;i1++){
-            for(int i2=0;i2<metic.length;i2++){
-                if(i1==i2){
-                    System.out.print(avg[i1]+"\t");
+        for(int i1=0;i1<average.length;i1++){
+            for(int i2=0;i2<average.length;i2++){
+                if(average[i1][i2]>0){
+                    System.out.print(average[i1][i2]+"\t");
                 }else{
-                    System.out.print(0+"\t");
+                    System.out.printf("%.0f\t",average[i1][i2]);
                 }
             }
             System.out.print("\n");
