b3c5c73e74dfc8c042f0aab1d650824dd5672081
Passapol <phasphlsuthahthrrm976@gmail.com>
2024-02-27 20:06:52 +0700

    update work week13

--- work/Week13_66011212067.java
+++ work/Week13_66011212067.java
@@ -1,17 +1,13 @@
-package work;
 import java.util.Scanner;
 class Week13_66011212067 {
     public static void main(String [] args){
         Week13_66011212067 input = new Week13_66011212067();
         System.out.println("***************************************************");
-        // String test = "1-2015-41462-23-4";
         String test = input.inputStr("input numberid : ");
         PeopleID peopleid = new PeopleID(test);
         peopleid.showall();
         System.out.println("***************************************************");
     }
-
-
     private String inputStr(String title){
         Scanner sc = new Scanner(System.in);
         System.out.print(title);
@@ -33,19 +29,25 @@ class PeopleID {
     public PeopleID(String id){
         this.staticid = id;
         id = id.replaceAll("-", "");
+        id = id.replaceAll(" ", "");
+        String chkstr = "0123456789";
         String []sub = id.split("");
         if(sub.length == 13){
+            boolean runstatus = true;
             for(int i=0;i<13;i++){
-                // System.out.println(Integer.parseInt(sub[i])+10);
-                this.id[i] = Integer.parseInt(sub[i]);
+                if(chkstr.indexOf("0") != -1){
+                    this.id[i] = Integer.parseInt(sub[i]); 
+                }else{
+                    break;
+                }
             }
-            step1();
-            step2();
-            step3();
-            step4();
-            step5();
-        }else{
-            System.out.println("ตัวเลขต้องมี 13 หลัก!!");
+            if(runstatus){
+                step1();
+                step2();
+                step3();
+                step4();
+                step5();
+            }
         }
     }
     void step1(){
